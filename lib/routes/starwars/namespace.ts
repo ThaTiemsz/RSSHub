ffe784f0a624e00e1f1b4b4f260a8fd7f6128028
Tiemen <Tiemen2@live.nl>
2026-06-09 07:41:29 +0200

    feat(route): add StarWars.com

---
+++ lib/routes/starwars/namespace.ts
@@ -0,0 +1,7 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'StarWars.com',
+    url: 'starwars.com',
+    lang: 'en',
+};
---
+++ lib/routes/starwars/news.ts
@@ -0,0 +1,58 @@
+import type { Data, DataItem, Route } from '@/types';
+import ofetch from '@/utils/ofetch';
+import { parseDate } from '@/utils/parse-date';
+
+export const route: Route = {
+    path: '/news',
+    categories: ['blog'],
+    example: '/starwars/news',
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['starwars.com/news'],
+            target: '/news',
+        },
+    ],
+    name: 'News Feed',
+    maintainers: ['Tiemen'],
+    handler,
+};
+
+async function handler(): Promise<Data> {
+    const data = await ofetch('https://www.starwars.com/_grill/more/news?r=1-4&l=24&o=0', {
+        headers: {
+            accept: 'application/json',
+        },
+    });
+
+    const items = data.map(
+        (item): DataItem => ({
+            title: item.title,
+            link: item.href,
+            description: item.text_content?.main_content,
+            pubDate: parseDate(item.content_date),
+            author: item.authors?.map((author) => ({
+                name: author.display_title || author.title,
+                url: author.href,
+                avatar: author.profile_image?.thumb_1x1?.src,
+            })),
+            category: item.category_labels ? Object.values<{ title: string; url: string }>(item.category_labels).map((label) => label.title) : undefined,
+            image: item.featured_image?.base_src,
+        })
+    );
+
+    return {
+        title: 'StarWars.com News',
+        link: 'https://www.starwars.com/news',
+        description: 'Star Wars News, Articles & Quizzes',
+        language: 'en',
+        item: items,
+    };
+}
